ddd71a425b3c94868c0df5092a8d08ea1fa0bf0c
JacobBraniger <46425131+JacobBraniger@users.noreply.github.com>
2022-07-12 15:56:01 -0400

    Added more errors, fixed setReason of func1&func2

--- HuskyAnalysisTool.py
+++ HuskyAnalysisTool.py
@@ -13,9 +13,9 @@ def func1(df):
     for i in range(len(df)):
         #making temporary list to hold and manipulate each row of data
         attributes = [df.at[i, 'Date/Time'], df.at[i, 'Type'], df.at[i, 'Source'], df.at[i, 'Description']]
-        for i in range(1, len(attributes)):
-            if (attributes[i][0] == ' '):
-                attributes[i] = attributes[i].replace(' ', '', 1)
+        for j in range(1, len(attributes)):
+            if (attributes[j][0] == ' '):
+                attributes[j] = attributes[j].replace(' ', '', 1)
 
         #Adding Machine Events
         if (attributes[1] == 'Machine'):
@@ -25,9 +25,9 @@ def func1(df):
         elif (attributes[1] == 'Cycle Interruption'):
             eventList.append(CycleInterruptEvent(attributes[0], attributes[1], attributes[2], attributes[3]))
             #
-            if(len(eventList) >= 2 and type(eventList[-2]) == MachineEvent):
-                if(eventList[-2].getType == "Cycle Interruption"):
-                    eventList[-2].setReason(eventList[i].getReason)
+            if(len(eventList) >= 2 and type(eventList[i-1]) == MachineEvent):
+                if(eventList[i-1].getPrevState() == "Cycle Interruption"):
+                    eventList[i-1].setReason(eventList[i].getReason())
 
         #If none of the above types, makes a generic event
         else:
@@ -51,12 +51,12 @@ def func2(df):
             eventList.append(MachineEvent.fiveLines(attributes[0], attributes[1], attributes[2], attributes[3], attributes[4]))
 
         #Adding Cycle Interruptions
-        elif (attributes[2] == 'Cycle Interruption'):
-            eventList.append(CycleInterruptEvent.fiveLines(attributes[0], attributes[1], attributes[2], attributes[3], attributes[4]))
+        elif (attributes[1] == 'Cycle Interruption'):
+            eventList.append(CycleInterruptEvent(attributes[0], attributes[1], attributes[2], attributes[3]))
             #
-            if(type(eventList[i-1]) == MachineEvent):
-                if(eventList[i-1].getType == "Cycle Interruption"):
-                    eventList[i-1].setReason(eventList[i].getReason)
+            if(len(eventList) >= 2 and type(eventList[i-1]) == MachineEvent):
+                if(eventList[i-1].getPrevState() == "Cycle Interruption"):
+                    eventList[i-1].setReason(eventList[i].getReason())
 
         #If none of the above types, makes a generic event
         else:
@@ -67,23 +67,30 @@ def func2(df):
 
 def analyze():
     #Creating data frame from selected file
-    workbook = filedialog.askopenfilename(parent=mainWin, initialdir= "/", title='Please select a directory')
-    df = pd.read_excel(workbook)
-
     try:
-        if (len(df.columns) == 4):
-            eventList = func1(df)
-        elif (len(df.columns) == 5):
-            eventList = func2(df)
-        else:
-            raise Exception
+        workbook = filedialog.askopenfilename(parent=mainWin, initialdir= "/", title='Please select a directory')
+        df = pd.read_excel(workbook)
+    except FileNotFoundError:
+        myLabel = Label(mainWin, text = '!!!File was not properly defined!!!', pady=5, fg='red')
+        myLabel.pack()
+        return
+    except ValueError:
+        myLabel = Label(mainWin, text = '!!!Not an excel file of the correct version. Please choose a .xlsx file!!!', pady=5, fg='red')
+        myLabel.pack()
+        return
     except Exception:
-        myLabel = Label(mainWin, text = '!!!File has abnormal amount of columns. Please delete events with excess data!!!', pady=5, fg='red')
+        myLabel = Label(mainWin, text = '!!!Something unexplicably went wrong!!!', pady=5, fg='red')
         myLabel.pack()
         return
 
-    #Making new text file to write to
-    f = open(e.get() + ".txt", "w")
+    if (len(df.columns) == 4):
+        eventList = func1(df)
+    elif (len(df.columns) == 5):
+        eventList = func2(df)
+    else:
+        myLabel = Label(mainWin, text = '!!!File has abnormal amount of columns. Please delete events with excess data!!!', pady=5, fg='red')
+        myLabel.pack()
+        return
 
     runTime = timedelta(0) #the total time that the machine has been running on auto cycle over the data period. Only counts if it was running for more than a minute
     problemList = [] #Counts up all reasons of Cycle Interruptions
@@ -101,18 +108,28 @@ def analyze():
             problemList.append(eventList[i].getReason())
     
     #Writing analytic data:
+    #Making new text file to write to
+    f = open(e.get() + ".txt", "w")
+    param = ['Start Time', 'End Time', 'Run Time (hr)', 'Availibility', 'Total Cycle Interruptions']
+    amount = [eventList[-1].getTime().strftime("%m/%d/%Y %I:%M:%S %p"), eventList[0].getTime().strftime("%m/%d/%Y %I:%M:%S %p"), str(round(runTime.total_seconds()/60/60, 3)), str(round(runTime / (eventList[0].getTime() - eventList[-1].getTime())*100, 2)) + "%", str(len(problemList))]
     #Writing total runtime
-    f.write("The total runtime over the period from " + eventList[-1].getTime().strftime("%m/%d/%Y %I:%M:%S %p") + " to " + eventList[0].getTime().strftime("%m/%d/%Y %I:%M:%S %p") + " was " + str(round(runTime.total_seconds()/60/60, 3)) + " hours.\n")
+    f.write("The total runtime over the period from " + amount[0] + " to " + amount[1] + " was " + amount[2] + " hours.\n")
     #Writing availibility
-    f.write("Availibility: " + str(round(runTime / (eventList[0].getTime() - eventList[-1].getTime())*100, 2)) + "%.\n")
+    f.write("Availibility: " + amount[3] + ".\n")
     #Writing total Cycle Interruptions
-    f.write("Total Cycle Interruptions: " + str(len(problemList)) + ".\n")
+    f.write("Total Cycle Interruptions: " + amount[4] + ".\n")
     #Writing Cycle Interruption breakdown
     new_vals = Counter(problemList).most_common() #Makes new counter and counts up the Cycle Interruption reasons
     new_vals = new_vals[::1] #this sorts the list in descending order
     for a, b in new_vals:
         f.write(str(a) + ": " + str(b) + ", " + str(round(b/len(problemList)*100, 2)) + "%\n")
+        param.append(str(a))
+        amount.append(b)
         #end
+    thisDict = {'Parameter': param, 'Amount': amount}
+    output = pd.DataFrame(thisDict)
+    output.to_excel(e.get() + '.xlsx', index=False)
+
     f.write("\n")
     #Writing log to file
     for i in range(len(eventList)-1):
@@ -124,21 +141,8 @@ def analyze():
         #Writing Cycle Interruptions
         elif (type(eventList[i]) == CycleInterruptEvent):
             f.write(str(eventList[i]) + "\n")
+    f.close #close file    
 
-    param = ['Start Time', 'End Time', 'Run Time (hr)', 'Availibility', 'Total Cycle Interruptions']
-    amount = [eventList[-1].getTime().strftime("%m/%d/%Y %I:%M:%S %p"), eventList[0].getTime().strftime("%m/%d/%Y %I:%M:%S %p"), str(round(runTime.total_seconds()/60/60, 3)), str(round(runTime / (eventList[0].getTime() - eventList[-1].getTime())*100, 2)) + "%", str(len(problemList))]
-    new_vals = Counter(problemList).most_common() #Makes new counter and counts up the Cycle Interruption reasons
-    new_vals = new_vals[::1] #this sorts the list in descending order
-    for a, b in new_vals:
-        param.append(str(a))
-        amount.append(b)
-        #end
-
-    thisDict = {'Parameter': param, 'Amount': amount}
-    output = pd.DataFrame(thisDict)
-    output.to_excel(e.get() + '.xlsx', index=False)
-
-    f.close #close file
     #Generate message that writing is done
     myLabel = Label(mainWin, text = "Generated " + e.get() + " from " + workbook, pady=5)
     myLabel.pack()
